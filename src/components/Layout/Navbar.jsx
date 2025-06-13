69a492e15530ae01c84373cb26012c6095fc1557
AlexSvet117 <alex.svet117@gmail.com>
2025-06-13 02:10:53 +0100

    Final changes

--- src/components/Layout/Navbar.jsx
+++ src/components/Layout/Navbar.jsx
@@ -1,19 +1,23 @@
- import { Link } from 'react-router';
-import './Navbar.css';
+import { Link } from "react-router";
+import "./Navbar.css";
 import { TbBooks } from "react-icons/tb";
 import { IoCartOutline } from "react-icons/io5";
 import { FaRegHeart } from "react-icons/fa6";
 import { IoSearch } from "react-icons/io5";
 
-
 function Navbar() {
   return (
     <nav className="playheaven-navbar navbar navbar-expand-lg navbar-dark">
       <div className="container-fluid px-4">
-
-        <a className="navbar-brand d-flex align-items-center" href="home">
-          <span className="playheaven-logo">Logo</span>
-        </a>
+        <Link
+          to="/home"
+          className="navbar-brand d-flex align-items-center text-decoration-none">
+          <img
+            src="/assets/play_heaven_logo (2).svg"
+            alt="Play Heaven Logo"
+            className="playheaven-logo-img"
+          />
+        </Link>
 
         <button
           className="navbar-toggler playheaven-toggler"
@@ -29,44 +33,66 @@ function Navbar() {
 
         <div className="collapse navbar-collapse" id="navbarNav">
           <div className="playheaven-nav-container d-flex w-100 align-items-center">
-
             <div className="flex-fill"></div>
-            
+
             <div className="flex-fill d-flex justify-content-center">
               <ul className="navbar-nav">
                 <li className="nav-item playheaven-nav-item">
-                  <a className="nav-link playheaven-nav-link" href="store">Store</a>
+                  <a className="nav-link playheaven-nav-link" href="store">
+                    Store
+                  </a>
                 </li>
                 <li className="nav-item playheaven-nav-item">
-                  <a className="nav-link playheaven-nav-link" href="categories">Categories</a>
+                  <a className="nav-link playheaven-nav-link" href="categories">
+                    Categories
+                  </a>
                 </li>
                 <li className="nav-item playheaven-nav-item">
-                  <a className="nav-link playheaven-nav-link" href="news">News</a>
+                  <a className="nav-link playheaven-nav-link" href="news">
+                    News
+                  </a>
                 </li>
                 <li className="nav-item playheaven-nav-item">
-                  <a className="nav-link playheaven-nav-link" href="about">About</a>
+                  <a className="nav-link playheaven-nav-link" href="about">
+                    About
+                  </a>
                 </li>
               </ul>
             </div>
-            
+
             <div className="flex-fill d-flex justify-content-end align-items-center">
-              <Link to="/store" className="playheaven-icon-btn text-decoration-none">
+              <Link
+                to="/store"
+                className="playheaven-icon-btn text-decoration-none"
+              >
                 <IoSearch />
               </Link>
-              
-              <Link to="/wishlist" className="playheaven-icon-btn text-decoration-none">
+
+              <Link
+                to="/wishlist"
+                className="playheaven-icon-btn text-decoration-none"
+              >
                 <FaRegHeart />
               </Link>
-              
-              <Link to="/cart" className="playheaven-icon-btn text-decoration-none">
+
+              <Link
+                to="/cart"
+                className="playheaven-icon-btn text-decoration-none"
+              >
                 <IoCartOutline />
               </Link>
-              
-              <Link to="/libraryPage" className="playheaven-icon-btn playheaven-library-btn text-decoration-none">
+
+              <Link
+                to="/libraryPage"
+                className="playheaven-icon-btn playheaven-library-btn text-decoration-none"
+              >
                 <TbBooks />
               </Link>
-              
-              <Link to="/profile" className="playheaven-user-profile d-flex align-items-center text-decoration-none">
+
+              <Link
+                to="/profile"
+                className="playheaven-user-profile d-flex align-items-center text-decoration-none"
+              >
                 <div className="playheaven-avatar"></div>
                 <span className="playheaven-username">PlayerOne</span>
               </Link>
